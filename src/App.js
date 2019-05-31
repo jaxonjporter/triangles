31a8fe07c0178953ab36e33b9afa7981238d06f2
Jaxon Porter <jaxonutah@gmail.com>
2019-05-31 16:01:24 -0600

    images were added for each triangle

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from 'react'
 import Triangles from './components/Triangles'
-import { Route, Switch } from 'react-router-dom'
+import { Route,} from 'react-router-dom'
 import Particles from "react-particles-js";
 import { particleParams, particleStyles } from "./styles/ParticleStyle";
 
 const App = () => {
   return (
     <>
-    <Particles params={particleParams} style={particleStyles} />
-    <Switch>
-      <Route exact pact='/triangles' component={Triangles} />
-      <Route exact pact='/purpose' component={Triangles} />
-      <Route exact pact='/home' component={Triangles} />
-    </Switch>
+      <Particles params={particleParams} style={particleStyles} />
+      <Route exact pact='/' component={Triangles} />
     </>
   
   )
--- src/components/Triangles.js
+++ src/components/Triangles.js
@@ -1,6 +1,10 @@
 import React, { useState, } from 'react'
 import TriangleForm from './TriangleForm'
 import styled, { css, } from 'styled-components';
+import equilateral from '../images/equilateral.png'
+import isoscelese from '../images/isoscelese.png'
+import scalene from '../images/scalene.png'
+import rightscalene from '../images/rightscalene.png'
 
 const Triangles = () => {
   const [triangle, setTriangle] = useState([])
@@ -10,8 +14,8 @@ const Triangles = () => {
       <Center>
 
         <DivExplain>
-          <h1>Triangle Type Calculator</h1>
-          <h3>This calculator will determine the type of triangle you have given the three sides. Enter the side lengths in the boxes below.</h3>
+          <Title>Triangle Type Calculator</Title>
+          <SubTitle>This calculator will determine the type of triangle given three sides. Enter the side lengths in the boxes below.</SubTitle>
         </DivExplain>
       </Center>
       <Center>
@@ -30,34 +34,62 @@ const Triangles = () => {
 const validateTriangle = (triangle) => {
   let tri = triangle.sort(function(a, b){return a-b})
   if (tri[0]+tri[1] > tri[2]){
-    return checkType(tri)
+    return determineType(tri)
   } else if (triangle.length > 0) {
-    return <h1>Invalid Triangle</h1>
+    return <h1>Invalid triangle</h1>
   } else {
-    return <h1>Please enter sides</h1>
+    return <h1>Please enter side lengths</h1>
   }
 }
 
-const checkType = (tri) => {
+const determineType = (tri) => {
   if (tri[0] === tri[1] && tri[1] === tri[2]){
-    return <h1>equilateral</h1>
+    return (
+      <>
+        <h1>Equilateral</h1>
+        <Img src={equilateral} />
+        <p>An equilateral triangle is defined by all three sides being the same length</p>
+      </>
+    )
   } else if (tri[0] === tri[1] || tri[0] === tri[2] || tri[1] === tri[2]){
     if (tri[0]**2 + tri[1]**2 === tri[2]**2){
-      return <h1>right Isoscelese</h1>
+      return (
+        <>
+          <h1>Right Isoscelese</h1>
+          <p>A right Isoscelese triangle has two equal side lengths, and one angle of 90 degrees</p>
+        </>  
+      )
     } else {
-      return <h1>Isoscelese</h1>
+      return (
+      <>
+        <h1>Isoscelese</h1>
+        <Img src={isoscelese} />
+        <p>An isoscelese triangle is defined by having 2 sides the same length</p>
+      </>
+      )
     }
   } else {
     if (tri[0]**2 + tri[1]**2 === tri[2]**2){
-      return <h1>right Scalene</h1>
+      return (
+        <>
+          <h1>Right Scalene</h1>
+          <Img src={rightscalene} />
+          <p>A right scalene triangle has all different side lengths and one angle of 90 degrees</p>
+        </>
+      )
     } else {
-      return <h1>scalene</h1>
+      return (
+        <>
+          <h1>Scalene</h1>
+          <Img src={scalene} />
+          <p>A scalene triangle is defined by having all different side lengths</p>
+        </>
+      )
     }
   }
 }
 
 const sizes = {
-  desktop: 992,
   tablet: 1068,
   phone: 606,
 }
@@ -72,7 +104,7 @@ const media = Object.keys(sizes).reduce((acc, label) => {
 }, {})
 
 const Container = styled.div`
-  margin: 0% 10%;
+  margin: 0 10%;
   font-family: 'Roboto', sans-serif;
 `
 
@@ -106,8 +138,12 @@ const DivExplain = styled.div`
   box-shadow: 2px 2px 5px #7f7f7f;
   border-radius: 5px;
   min-height: 10vh;
-  margin: 0px 5px 5px 5px;
+  margin: 40px 5px 5px 5px;
   text-align: center;
+
+  ${media.tablet`
+    margin-top: 0px;
+  `}
 
 
 `
@@ -123,5 +159,16 @@ const Center = styled.div`
 `}
 `
 
+const Img = styled.img`
+  width: 200px;
+`
+
+const Title = styled.h1`
+  margin: 10px 0 0 0;
+`
+const SubTitle = styled.h3`
+  margin-top: 10px;
+  margin-bottom: 5px;
+`
 
 export default Triangles
